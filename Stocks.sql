20b0d36ac1aced8c8095190e20dfc8c98c80ce0c
Robertoq123 <rsquezad@asu.edu>
2025-10-09 13:01:35 -0700

    The market hours were put in. Stocks were put into a table.

---
+++ Stocks.sql
@@ -0,0 +1,5 @@
+INSERT INTO StockInventory (name, ticker, quantity, initStockPrice, currentMarketPrice)
+VALUES 
+  ('Dell', 'DELL', 500, 100, 105),
+  ('Hp', 'HP', 500, 100, 105)
+AS new_stockportfoliostockinventory
