4ed6014fbe4b3b2b67435bffae78c009f0b3afa2
Robertoq123 <rsquezad@asu.edu>
2025-11-22 12:23:47 -0700

    Config file

---
+++ config.py
@@ -0,0 +1,7 @@
+import os 
+DB_USERNAME = "admin"
+DB_PASSWORD = "password"
+DB_HOST = "webproject.caby8a6uuav7.us-east-1.rds.amazonaws.com"
+DB_PORT = "3306"
+DB_NAME = "webproject"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://admin/password@webproject.caby8a6uuav7.us-east-1.rds.amazonaws.com:3306/webproject"
